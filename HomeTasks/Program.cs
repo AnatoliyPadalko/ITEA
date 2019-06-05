95bb79343e77587cf03c0aeffcbfcf0e5e107156
Anatoliy Padalko <anatoliy.padalko@hotmail.com>
2019-06-05 10:37:55 +0300

    End - (Without view)

--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -14,34 +14,61 @@ namespace HomeTasks
             string user;
             char[] field = new char[10];
             int dog = 0;
+            int hp = 100;
+            Random rnd = new Random();
             Boolean countdog = false;
+            int bomb = 0;
+            int aid = 0;
+            do
+            {
+                bomb = rnd.Next(1, 8);
+                aid = rnd.Next(1, 8);
+            } while (bomb == aid);
+
+            field[bomb] = '*';
+            field[aid] = '+';
+
             do
             {
                 for (int i = 0; i < field.Length; i++)
                 {
-                    if (countdog == false && i==dog)
+                    
+                    if (countdog == false && i==dog && !(i==bomb) && !(i==aid))
                     {
                         field[dog] = '@';
                         countdog = true;
                     }
-                    else
+                    else if(!(i == bomb)&& !(i == aid))
                     {
                         field[i] = '_';
                     }
                     Console.Write(field[i]);
                 }
+                Console.Write("Helth = "+hp);
                 user = Console.ReadLine();
+               
                 if (user == "d")
                 {
                     dog++;
                     countdog = false;
+                    hp = hp - 5;
                 }
                 else if (user == "a")
                 {
                     dog--;
                     countdog = false;
+                    hp = hp - 5;
                 }
-            } while (dog < 10);
+                if(dog==bomb)
+                {
+                    hp = hp - 40;
+                }
+                if (dog == aid)
+                {
+                    hp = hp + 40;
+                }
+            } while (dog >= 0 && hp>0 && dog<10);
+          
         }
     }
 }
