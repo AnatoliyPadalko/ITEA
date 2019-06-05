9690223d101cc70c6498db23211c01050f2a51f4
Anatoliy Padalko <anatoliy.padalko@hotmail.com>
2019-06-05 15:50:08 +0300

    not working

--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -12,78 +12,106 @@ namespace HomeTasks
         {
 
             string user;
-            char[] field = new char[10];
+            char[][] field = new char[10][];
             int dog = 0;
+            char[] temp = new char[10];
+            int dogEx = 0;
             int hp = 100;
             Random rnd = new Random();
             Boolean countdog = false;
-            int bomb = 0;
-            int aid = 0;
-            do
-            {
-                bomb = rnd.Next(1, 8);
-                aid = rnd.Next(1, 8);
-            } while (bomb == aid);
-
-            field[bomb] = '*';
-            field[aid] = '+';
-
-            do
-            {
+            int[] bomb = new int [10];
+            int[] aid = new int [10];
+           
+            
+              do
+                  {
 
 
-                Console.Write("\b");
-                for (int i = 0; i < field.Length; i++)
-                {
-                    
-                    if (countdog == false && i==dog && !(i==bomb) && !(i==aid))
-                    {
-                        field[dog] = '@';
-                        countdog = true;
-                    }
-                    else if(!(i == bomb)&& !(i == aid))
-                    {
-                        field[i] = '_';
-                    }
- 
-                }
-               
+                      for(int i=0;i<field.Length;i++)
+                      {
+                        do
+                        {
+                            bomb[i] = rnd.Next(1, 8);
+                            aid[i] = rnd.Next(1, 8);
+                        } while (bomb[i] == aid[i]);
+                        for (int j = 0; j < temp.Length; j++)
+
+                        {
+                   
+                        if (countdog == false && i == dogEx && j == dog && !(j == bomb[j]) && !(j == aid[j]))
+                            {
+                            temp[dog] = '@';
+                            countdog = true;
+                            }
+                            if (!(j == bomb[i]) && !(j == aid[i]) && i != dogEx && j != dog)
+                            {
+                            temp[j] = '_';
+                            }
+                            if (j == bomb[i])
+                            {
+                                temp[j] = '*';
+                            }
+                            if (j == aid[i])
+                            {
+                                temp[j] = '+';
+                            }
+                       
+                        }
+                       field[i] = temp;
+                      }
+             
+
                 Console.Clear();
-                for (int i = 0; i < field.Length; i++)
-                {
-                    Console.Write(field[i]);
-                    
-                }
-                Console.Write("\tHelth = " + hp + "");
-                
+
+                  for (int i = 0; i < field.Length; i++)
+                  {
+
+                      for (int j = 0; j < field.Length; j++)
+                      {
+                          Console.Write(field[i][j]);
+                      }
+                      Console.WriteLine();
+                  }
+                  Console.Write("\tHelth = " + hp + "");
+                  user = Console.ReadLine();
+                      if (user == "d")
+                      {
+                          dog++;
+                          countdog = false;
+                          hp = hp - 5;
+                      }
+                      else if (user == "a")
+                      {
+                          dog--;
+                          countdog = false;
+                          hp = hp - 5;
+                      }
+                  if (user == "w")
+                  {
+                      dogEx++;
+                      countdog = false;
+                      hp = hp - 5;
+                  }
+                  if (user == "x")
+                  {
+                      dogEx--;
+                      countdog = false;
+                      hp = hp - 5;
+                  }
+                  if (dog == bomb[dogEx])
+                      {
+                          hp = hp - 40;
+                      }
+                      if (dog == aid[dogEx])
+                      {
+                          hp = hp + 40;
+                      }
+                      if (hp > 100)
+                      {
+                          hp = 100;
+                      }
+                  } while (dog >= 0 && hp > 0 && dog < 10 && dogEx>=0 && dogEx<10);
               
-                user = Console.ReadLine();
-                if (user == "d")
-                {
-                    dog++;
-                    countdog = false;
-                    hp = hp - 5;
-                }
-                else if (user == "a")
-                {
-                    dog--;
-                    countdog = false;
-                    hp = hp - 5;
-                }
-                if(dog==bomb)
-                {
-                    hp = hp - 40;
-                }
-                if (dog == aid)
-                {
-                    hp = hp + 40;
-                }
-                if (hp > 100)
-                {
-                    hp = 100;
-                }
-            } while (dog >= 0 && hp>0 && dog<10);
-          
         }
     }
 }
